43b9cd560d101775b7e262ece6b90d244378ef84
emya <e.ayada810004@gmail.com>
2017-08-22 15:54:28 -0700

    add tests

--- lib/etl/output/redshift.rb
+++ lib/etl/output/redshift.rb
@@ -219,7 +219,12 @@ SQL
       end
     end
 
-     # Runs the ETL job
+    def trim_newline(input)
+      return input unless input.is_a? String
+      input.gsub("\n", " ")
+    end
+
+    # Runs the ETL job
     def run_internal
       rows_processed = 0
       msg = ''
@@ -235,14 +240,7 @@ SQL
         reader.each_row do |erow|
           # Remove line break from all columns so that Redshift can detect delimiter
           # Need this method since ESCAPE option with COPY command on Redshift does not work
-          row = erow.each_with_object({}) do |r, h|
-            if r[1].is_a? String
-              v = r[1].gsub("\n", " ")
-              h[r[0].to_s] = v
-            else
-              h[r[0].to_s] = r[1]
-            end
-          end
+          row = erow.each_with_object({}) { |r, h| h[r[0].to_s] = trim_newline(r[1]) }
 
           if schema && !schema.columns.empty? 
             s = schema.columns.keys.map { |k| row[k.to_s] }
