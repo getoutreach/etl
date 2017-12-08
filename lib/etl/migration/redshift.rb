2daa19b9d836318701ac099413698656e5419ee4
emya <e.ayada810004@gmail.com>
2017-12-07 16:10:17 -0800

    remove conn params

--- lib/etl/migration/redshift.rb
+++ lib/etl/migration/redshift.rb
@@ -4,8 +4,6 @@ module ETL::Migration
   # Base class for all jobs that are run
   class Redshift
     def initialize(client)
-      conn_params = ETL.config.redshift.fetch(:etl, { host: "localhost", port: 5439, user: "masteruser", password: "password" })
-      aws_params = ETL.config.aws.fetch(:etl)
       @client = client
     end
 
