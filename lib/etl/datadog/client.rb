ffbffc19f96bedd2987acbf0def6385099b86c9d
Jiwei Cao <jiwei.cao@outreach.io>
2018-06-15 11:43:11 -0700

    Add message titile and alert

--- lib/etl/datadog/client.rb
+++ lib/etl/datadog/client.rb
@@ -5,25 +5,28 @@ module ETL::Datadog
   class Client
     attr_accessor :host_name
 
-    def self.create_instance()
-      ::ETL::Datadog::Client.new()
+    def self.create_instance
+      ::ETL::Datadog::Client.new
     end
 
-    def initialize()
+    def initialize
       api_key = ENV['DATADOG_API_KEY']
       @host_name = ENV['ETL_DATADOG_URL']
-      if api_key.nil?
-        @client = nil
-      else
-        @client = Dogapi::Client.new(api_key)
-      end
+      @client = if api_key.nil?
+                  nil
+                else
+                  Dogapi::Client.new(api_key)
+                end
     end
 
-    def send_event(message)
+    def send_event(message, message_title, alert_type, tags = ['legacy_etl'])
       unless @host_name.nil?
-        @client.emit_event(Dogapi::Event.new(message), :host => @host_name)
+        @client.emit_event(Dogapi::Event.new(message,
+                                             msg_title: message_title,
+                                             alert_type: alert_type,
+                                             tags: tags),
+                           host: @host_name)
       end
     end
-
   end
 end
